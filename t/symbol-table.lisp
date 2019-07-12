bb36647330a15ee20d9bf7c8acac828ca5840299
Brad Svercl <bradsvercl@gmail.com>
2019-07-11 21:19:36 -0500

    Basic symbol table test

--- t/symbol-table.lisp
+++ t/symbol-table.lisp
@@ -1,3 +1,11 @@
 ;;;; symbol-table.lisp
 
 (in-package :woo/tests)
+
+(test lookup-symbols
+  (let* ((inner (woo::make-symbol-table))
+         (outer (woo::make-symbol-table inner)))
+    (woo::set-symbol inner "inner" "inside")
+    (woo::set-symbol outer "outer" "outside")
+    (is (null (woo::lookup-symbol inner "outside")))
+    (is (equal "inside" (woo::lookup-symbol outer "inner")))))
