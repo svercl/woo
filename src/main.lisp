96c85aeadc543214446a442c95e75df7af5490ff
Brad Svercl <bradsvercl@gmail.com>
2019-08-01 09:51:58 -0500

    Or not

--- src/main.lisp
+++ src/main.lisp
@@ -9,38 +9,35 @@
 
 (defun parse-string (text)
   "Take a string and parse it."
-  (serapeum:~> text
-               (make-lexer)
-               (make-parser)
-               (parse-program)))
+  (let* ((lexer (make-lexer text))
+         (parser (make-parser lexer)))
+    (parse-program parser)))
 
 (defun evaluate-string (text &optional outer)
-  (let ((env (or outer (make-environment))))
-    (serapeum:~> text
-                 (parse-string)
-                 (evaluate _ env)
-                 (inspect-object))))
+  (let* ((env (or outer (make-environment)))
+         (program (parse-string text))
+         (evaluated (evaluate program env)))
+    (inspect-object evaluated)))
 
 (defun rpl ()
   "Read print loop."
   (loop
     (fresh-line)
-    (serapeum:~> (prompt-read-line)
-                 (parse-string)
-                 (pprint))))
+    (let* ((line (prompt-read-line))
+           (parsed (parse-string line)))
+      (pprint parsed))))
 
 (defun repl ()
   "Read evaluate print loop."
   (let ((env (make-environment)))
     (loop
       (fresh-line)
-      (serapeum:~> (prompt-read-line)
-                   (evaluate-string _ env)
-                   (princ)))))
+      (let* ((line (prompt-read-line))
+             (evaluated (evaluate-string line env)))
+        (princ evaluated)))))
 
 (defun rep-file (pathname)
   "Read evaluate and print file."
-  (serapeum:~> pathname
-               (alexandria:read-file-into-string)
-               (evaluate-string)
-               (princ)))
+  (let* ((file (alexandria:read-file-into-string pathname))
+         (evaluated (evaluate-string file)))
+    (princ evaluated)))
